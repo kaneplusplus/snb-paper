1afa33084145f8ed180b5b2cacd181f1d5fa9f4f
Michael Kane <kaneplusplus@gmail.com>
2016-02-29 12:25:09 -0500

    Stat and Prob Letter draft started.

--- paper-1/script.r
+++ paper-1/script.r
@@ -72,9 +72,6 @@ flips_to_kplot_df = function (flips) {
     d
 }
 
-
-
-
 X$head <- c(0, cumsum(flips))
 X$tail<- c(0, cumsum(!(flips)))
 X$headEnd <- c(X$head[-1], NA)
@@ -82,26 +79,101 @@ X$tailEnd <- c(X$tail[-1], NA)
 
 X2 <- X[1:(min(which(X$headEnd >= s), which(X$tailEnd >= t))),]
 
-p2 <- ggplot(data=X2) + #right, up, data=X) + #, geom="segment") +
-  scale_x_continuous(breaks=0:t, limits=c(0, t)) +
-  scale_y_continuous(breaks=0:s, limits=c(0, s), labels=as.character(0:s)) +
-  geom_segment(mapping=aes(x=tail, y=head, xend=tailEnd,
-    yend=headEnd), arrow=arrow()) +
-  geom_segment(x=0, y=s, xend=t-1, yend=s, color="red") +
-  geom_segment(x=t, y=0, xend=t, yend=s-1, color="green")
+#p2 <- ggplot(data=X2) + #right, up, data=X) + #, geom="segment") +
+#  scale_x_continuous(breaks=0:t, limits=c(0, t)) +
+#  scale_y_continuous(breaks=0:s, limits=c(0, s), labels=as.character(0:s)) +
+#  geom_segment(mapping=aes(x=tail, y=head, xend=tailEnd,
+#    yend=headEnd), arrow=arrow()) +
+#  geom_segment(x=0, y=s, xend=t-1, yend=s, color="red") +
+#  geom_segment(x=t, y=0, xend=t, yend=s-1, color="green")
 
-ggsave("ZeltermanPlot.pdf", p2, width=7, height=5)
-
+#ggsave("ZeltermanPlot.pdf", p2, width=7, height=5)
 
 p3 = kplot( c(rep(0, 3), 1, rep(0, 6), 1), s=2, t=11) + 
   xlab("Number of Patients Enrolled") + ylab("Number of Responders") +
   geom_text(data=NULL, x=7, y=2.1, label="Success Boundary") +
   geom_text(data=NULL, x=12, y=0.5, label="Failure Boundary", 
-            angle=80)
-
-
+            angle=(45+90)/2+9)
 
 ggsave("KanePlot.pdf", p3, width=7, height=5)
+
+# Regions s < t
+s = 3
+t = 5
+flips = rep(0., t)
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length=n+1)
+  hcl(h=hues, l=65, c=100)[1:n]
+}
+rb = gg_color_hue(3)
+
+x = data.frame(list(x = c(s, t, t, s), y = c(0, 0, s, s), value=2, id="s"))
+x = rbind(x, data.frame(list(x = c(t, s+t-1, s+t-1, t), 
+                          y = c(0, s-1, s, s), value=1, id="st")))
+x$id = factor(x$id, levels=c("s", "t", "st"))
+
+p4 = ggplot(x, aes(x=x, y=y, group=id)) + 
+  geom_polygon(aes(fill=id, id = id), alpha=0.4) + 
+  scale_fill_manual(values=c("s"=rb[1], "t"=rb[2], "st"=rb[3])) +
+  theme(legend.position="none") +
+  geom_segment(x=s, y=s, xend=(t+s-1), yend=s, color="green", linetype=1,
+               size=1) +
+  geom_segment(x=t, y=0, xend=(s+t-1), yend=s-1, col="red", size=1) +
+  annotate("text", x=4, y=2.25, label="S(k, p, s)") +
+  annotate("text", x=6, y=2.25, label="S(k, p, s) + S(k, 1-p, t)") + 
+  ylab("Number of Responders") + xlab("Number of Patients Enrolled")
+  
+ggsave("Region-s-lt-t.pdf", p4, width=7, height=5)
+
+# Region s > t
+s = 5
+t = 3
+flips = rep(0., t)
+x = data.frame(list(x = c(s, s+t-1, s+t-1, s), 
+                    y = c(s-t, s-1, s, s), value=3, id="st"))
+x = rbind(x, data.frame(list(x = c(t, s, s, t), 
+                          y = c(0, s-t, s, s), value=1, id="t")))
+x$id = factor(x$id, levels=c("s", "t", "st"))
+
+p5 = ggplot(x, aes(x=x, y=y, group=id)) + 
+  geom_polygon(aes(fill=id, id = id), alpha=0.4) + 
+  scale_fill_manual(values=c("s"=rb[1], "t"=rb[2], "st"=rb[3])) +
+  theme(legend.position="none") +
+  geom_segment(x=s, y=s, xend=(t+s-1), yend=s, color="green", linetype=1,
+               size=1) +
+  geom_segment(x=t, y=0, xend=(s+t-1), yend=s-1, col="red", size=1) +
+  annotate("text", x=4, y=4, label="S(k, 1-p, t)") +
+  annotate("text", x=6, y=4, label="S(k, p, s) + S(k, 1-p, t)") + 
+  ylab("Number of Responders") + xlab("Number of Patients Enrolled")
+ggsave("Region-s-gt-t.pdf", p5, width=7, height=5)
+
+# Region s = t
+s = 5
+t = 5
+x = data.frame(list(x = c(s, s+t-1, s+t-1, s), 
+                    y = c(0, s-1, s, s), value=1, id="st"))
+x$id = factor(x$id, levels=c("s", "t", "st"))
+p6 = ggplot(x, aes(x=x, y=y, group=id)) + 
+  geom_polygon(aes(fill=id, id=id), alpha=0.4) + 
+  scale_fill_manual(values=c("s"=rb[1], "t"=rb[2], "st"=rb[3])) +
+  theme(legend.position="none") +
+  geom_segment(x=s, y=s, xend=(t+s-1), yend=s, color="green", linetype=1,
+               size=1) +
+  geom_segment(x=t, y=0, xend=(s+t-1), yend=s-1, col="red", size=1) +
+  annotate("text", x=7, y=4, label="S(k, p, s) + S(k, 1-p, t)") +
+  ylab("Number of Responders") + xlab("Number of Patients Enrolled")
+ggsave("Region-s-eq-t.pdf", p6, width=7, height=5)
+
+
+stop("here")
+
+d = snb:::flips_to_kplot_df(flips)
+qplot(k, path, data = d, geom = "blank") +
+  scale_x_continuous(breaks = 0:(t + s), limits = c(0, t + s)) +
+  scale_y_continuous(breaks = 0:s, limits=c(0, s+0.15)) +
+  geom_segment(x=s, y=s, xend=(t+s-1), yend=s, color="green", linetype=1) +
+  geom_segment(x=t, y=0, xend=(s+t-1), yend=s-1, col="red") 
+
 
 s=2
 t=11
