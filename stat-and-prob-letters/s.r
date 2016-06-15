3840defa5bf86e2d5a1aebbf2578b42991d2562d
Michael Kane <kaneplusplus@gmail.com>
2016-06-15 15:25:38 -0400

    More changes based on Dan’s comments.

--- stat-and-prob-letters/s.r
+++ stat-and-prob-letters/s.r
@@ -3,49 +3,50 @@ library(ggplot2)
 
 ## kplot
 
-kplot = function(flips, s, t) {
-  if (!is.list(flips)) {
-    d = flips_to_kplot_df(flips)
-    p = qplot(k, path, data = d, geom = "line") +
-      scale_x_continuous(breaks = 0:(t + s), limits = c(0, t + s - 1)) +
-      scale_y_continuous(breaks = 0:s, limits=c(0, s+0.15)) +
-      geom_segment(x=s, y=s, xend=(t+s-1), yend=s, linetype=2) +
-      geom_segment(x=t, y=0, xend=(s+t-1), yend=s-1, linetype=2)
-#      geom_segment(x=s, y=s, xend=(t+s-1), yend=s, color="green", linetype=1) +
-#      geom_segment(x=t, y=0, xend=(s+t-1), yend=s-1, col="red")
-  } else {
-    flip_set = lapply(flips, flips_to_kplot_df)
-    for (i in 1:length(flip_set)) {
-      flip_set[[i]]$num = as.factor(i)
-      flip_set[[i]]$k = jitter(flip_set[[i]]$k)
-      flip_set[[i]]$k[flip_set[[i]]$k < 0] = 0
-    }
-    d = Reduce(rbind, flip_set)[, -(4:5)]
-    p = qplot(k, path, data = d, geom = "path", group = num) +
-        scale_x_continuous(breaks=0:(t+s), limits = c(0, t+s)) +
-        geom_segment(x = s, y = s, xend = (t + s - 1), yend = s,
-                     linetype=2) +
-        geom_segment(x=t, y=0, xend=(s+t-1), yend=s-1, linetype=2)
+#kplot = function(flips, s, t) {
+#  if (!is.list(flips)) {
+#    d = flips_to_kplot_df(flips)
+#    p = qplot(k, path, data = d, geom = "line") +
+#      scale_x_continuous(breaks = 0:(t + s), limits = c(0, t + s - 1)) +
+#      scale_y_continuous(breaks = 0:s, limits=c(0, s+0.15)) +
+#      geom_segment(x=s, y=s, xend=(t+s-1), yend=s, linetype=2) +
+#      geom_segment(x=t, y=0, xend=(s+t-1), yend=s-1, linetype=2)
+##      geom_segment(x=s, y=s, xend=(t+s-1), yend=s, color="green", linetype=1) +
+##      geom_segment(x=t, y=0, xend=(s+t-1), yend=s-1, col="red")
+#  } else {
+#    flip_set = lapply(flips, flips_to_kplot_df)
+#    for (i in 1:length(flip_set)) {
+#      flip_set[[i]]$num = as.factor(i)
+#      flip_set[[i]]$k = jitter(flip_set[[i]]$k)
+#      flip_set[[i]]$k[flip_set[[i]]$k < 0] = 0
+#    }
+#    d = Reduce(rbind, flip_set)[, -(4:5)]
+#    p = qplot(k, path, data = d, geom = "path", group = num) +
+#        scale_x_continuous(breaks=0:(t+s), limits = c(0, t+s)) +
 #        geom_segment(x = s, y = s, xend = (t + s - 1), yend = s,
-#                     color = "green") +
-#        geom_segment(x=t, y=0, xend=(s+t-1), yend=s-1, col="red")
-    p
-  }
-  p
-}
+#                     linetype=2) +
+#        geom_segment(x=t, y=0, xend=(s+t-1), yend=s-1, linetype=2)
+##        geom_segment(x = s, y = s, xend = (t + s - 1), yend = s,
+##                     color = "green") +
+##        geom_segment(x=t, y=0, xend=(s+t-1), yend=s-1, col="red")
+#    p
+#  }
+#  p
+#}
 
-flips_to_kplot_df = function (flips) {
-  d = data.frame(k = 0:length(flips))
-  d$head = c(0, cumsum(flips))
-  d$tail = c(0, cumsum(1 - flips))
-  d$headEnd = c(d$head[-1], NA)
-  d$tailEnd = c(d$tail[-1], NA)
-  d$path = c(0, cumsum(flips))
-  d$k = 0:(nrow(d) - 1)
-  d
-}
+#flips_to_kplot_df = function (flips) {
+#  d = data.frame(k = 0:length(flips))
+#  d$head = c(0, cumsum(flips))
+#  d$tail = c(0, cumsum(1 - flips))
+#  d$headEnd = c(d$head[-1], NA)
+#  d$tailEnd = c(d$tail[-1], NA)
+#  d$path = c(0, cumsum(flips))
+#  d$k = 0:(nrow(d) - 1)
+#  d
+#}
 
-p = kplot( c(rep(0, 3), 1, rep(0, 6), 1), s=2, t=11) +
+
+p = kplot( c(rep(0, 3), 1, rep(0, 6), 1), s=2, t=11, bw=TRUE) +
   xlab("Number of Patients Enrolled") + 
   ylab("Number of Responders to Treatment") +
   geom_text(data=NULL, x=7, y=2.1, label="Success Boundary") +
@@ -61,7 +62,7 @@ ggsave("KanePlot.pdf", p, width=10, height=3.5)
 
 approx_plots_start = list(
   dsnb_stack_plot(0.2, 15, 75) + scale_x_discrete(breaks=seq(15, 95, by=10)) +
-    labs(x="", y="", title="bimodal example (0.2, 15, 75)") + theme_bw() +
+    labs(x="", y="", title="bimodal (0.2, 15, 75)") + theme_bw() +
     scale_fill_grey(),
   dsnb_stack_plot(0.35, 50, 50) + scale_x_discrete(breaks=seq(50, 105, by=10))+
     labs(x="", y="", title="approximate normal (0.35, 50, 50)") + theme_bw() +
@@ -105,11 +106,12 @@ dev.off()
 # Binomial Tail Connection.
 p = 0.045
 s = 2
-t = 12
+t = 11
 n = s+t-1
 
-dsnb_stack_plot(p, s, t) + ylab("SNB(0.045, 2, 12)") + theme_bw() + 
-  scale_fill_grey()
+plot.new()
+dsnb_stack_plot(p, s, t) + title("SNB(0.045, 2, 11)") + theme_bw() + 
+  scale_fill_grey() + ylab("Probability") + ylim(0, 0.6) 
 ggsave("snb_density.pdf")
 
 s1 = dsnb_stacked(min(s,t):n, p, s, t)
@@ -120,18 +122,20 @@ d = data.frame(list(x=0:n, y=b1))
 d$Outcome = factor(c(rep("t", s), rep("s", n-s+1)))
 d$Outcome = relevel(d$Outcome, "s")
 
-ggplot(d, aes(x=x, y=y, fill=Outcome))+geom_bar(stat="identity") + xlab("k")+
-  ylab("Bin(0.045, 13)") + theme_bw() + scale_fill_grey()
+ggplot(d, aes(x=factor(x), y=y, fill=Outcome)) +
+  geom_bar(stat="identity") + xlab("k")+
+  title("Bin(0.045, 12)") + theme_bw() + scale_fill_grey() +
+  ylab("") + ylim(0, 0.6) 
 ggsave("bin_density.pdf")
 
 # Sample size and variance for posterior distribution.
 s = 2
-t = 12
+t = 11
 p = 0.045
-x1 = cdsnb_stacked(min(s, t):(s+t-1), s, t, c(0.045, 0.955))
+x1 = cdsnb_stacked(min(s, t):(s+t-1), c(0.045, 0.955), s, t)
 stacked_plot(x1$k, x1$s, x1$t)
 dev.new()
-x2 = cdsnb_stacked(min(s, t):(s+t-1), s, t, c(0.045*5, 0.955*5))
+x2 = cdsnb_stacked(min(s, t):(s+t-1), c(0.045*5, 0.955*5), s, t)
 stacked_plot(x2$k, x2$s, x2$t)
 dev.new()
 plot(dbeta(seq(0, 1, by=0.01), 0.045*5, 0.955*5), type="l", col="green")
@@ -140,7 +144,7 @@ lines(dbeta(seq(0, 1, by=0.01), 0.045, 0.955), type="l", col="red")
 
 ecsnb(c(0.5, 0.5), s, t)
 
-x2 = cdsnb_stacked(min(s, t):(s+t-1), s, t, c(0.1*10, 0.9*10))
+x2 = cdsnb_stacked(min(s, t):(s+t-1), c(0.1*10, 0.9*10), s, t)
 stacked_plot(x2$k, x2$s, x2$t)
 
 # Probality of success in Bayesian model.
@@ -157,20 +161,42 @@ names(x) = c("n", "e", "v")
 exp_size = esnb(0.045, s, t)
 ggplot(x, aes(x=n, y=e)) + geom_line() + 
   geom_segment(y=exp_size,x=min(is),yend=exp_size, xend=max(is), color="grey") +
-  ylim(range(c(x$e, exp_size, 12))) +
-  geom_text(data=NULL, x=50, y=11.98, label="Deterministic Limit (11.96)") + 
-    theme_bw() + scale_fill_grey() + ylab("Expected Posterior") + xlab("c")
+  ylim(range(c(x$e, exp_size, 11))) +
+  geom_text(data=NULL, x=50, y=11.02, label=paste0("Deterministic Limit (", 
+    sprintf("%0.2f", exp_size), ")")) + 
+  theme_bw() + scale_fill_grey() + ylab("Compound Distribution Mean") + 
+  xlab("c")
+
 ggsave("bayesian-sample-expectation.pdf")
 
 exp_var = vsnb(0.045, s, t)
 ggplot(x, aes(x=n, y=v)) + geom_line() + 
   geom_segment(y=exp_var,x=min(is),yend=exp_var, xend=max(is), color="grey") +
   ylim(range(c(x$v, exp_var))) +
-  geom_text(data=NULL, x=50, y=2.5, label="Deterministic Limit (2.43)") + 
-    theme_bw() + scale_fill_grey() + ylab("Variance of the Posterior") + xlab("c")
+  geom_text(data=NULL, x=50, y=1.82, 
+    label=paste0("Deterministic Limit (", sprintf("%0.2f", exp_var), ")")) + 
+    theme_bw() + scale_fill_grey() + ylab("Compound Distribution Variance") + xlab("c")
 ggsave("bayesian-sample-variance.pdf")
 
 # Expected CSNB size
 # Variance of CSNB size
 # a) b)
+
+dsnb_plot(p, s, t) + ylab("Probability")
+ggsave("snb-first-plot.pdf")
+
+ps = seq(0, 1, by=0.01)
+moments = foreach(p=ps, .combine=rbind) %do% {
+  c(esnb(p, s, t), vsnb(p, s, t))
+}
+
+m = as.data.frame(cbind(ps, moments))
+names(m) = c("p", "exp", "var")
+rownames(m) = NULL
+
+ggplot(m, aes(x=p, y=exp)) + geom_line() + ylab("Distribution Mean")
+ggsave("dist-mean.pdf")
+
+ggplot(m, aes(x=p, y=var)) + geom_line() + ylab("Distribution Variance")
+ggsave("dist-var.pdf")
 
